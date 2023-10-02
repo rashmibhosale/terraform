b84f2f530a13d0333bf16800886f974ad8d83be2
Rajeev Ranjan <Rajeev.Ranjan@cornerstone-bb.com>
2023-10-02 13:36:35 +0530

    code formated

--- terraform-bhs/main.tf
+++ terraform-bhs/main.tf
@@ -1,16 +1,16 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "0.14.8"
     }
   }
   # Defining the location remote satate file, so that multiple user can work together.
   backend "azurerm" {
-    resource_group_name   = "backend-rg"
-    storage_account_name  = "terrastatebackend01"
-    container_name        = "terraformstate"
-    key                   = "bhs.terraform.tfstate"
+    resource_group_name  = "backend-rg"
+    storage_account_name = "terrastatebackend01"
+    container_name       = "terraformstate"
+    key                  = "bhs.terraform.tfstate"
   }
 }
 provider "azurerm" {
@@ -18,27 +18,27 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg" {
-    name = var.resource_group_name
-    location = var.location
+  name     = var.resource_group_name
+  location = var.location
 }
 
 resource "azurerm_virtual_network" "vnet" {
-    name = var.vnet_name
-    resource_group_name = azurerm_resource_group.rg.name
-    location = azurerm_resource_group.rg.location
-    address_space = [ "10.0.0.0/16" ]
+  name                = var.vnet_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  address_space       = ["10.0.0.0/16"]
 }
 
 resource "azurerm_subnet" "subnet" {
-    name = var.subnet_name
-    resource_group_name = azurerm_resource_group.rg.name
-    virtual_network_name = azurerm_virtual_network.vnet.name
-    address_prefixes = [ "10.0.1.0/24" ]
+  name                 = var.subnet_name
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
 }
 
 resource "azurerm_public_ip" "pubip" {
-    name = var.publicip_name
-    location = azurerm_resource_group.rg.location
-    resource_group_name = azurerm_resource_group.rg.name
-    allocation_method = "Static"
+  name                = var.publicip_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
 }
--- terraform-bhs/variable.tf
+++ terraform-bhs/variable.tf
@@ -1,26 +1,26 @@
 variable "resource_group_name" {
-  default = "bhs-assignment-rg-01"
+  default     = "bhs-assignment-rg-01"
   description = "This resource group is used to create resource for BHS"
-  type = string
+  type        = string
 }
 
 variable "location" {
   default = "South India"
-  type = string
+  type    = string
 }
 
 variable "vnet_name" {
-  type = string
+  type    = string
   default = "bhs-vnet-01"
 }
 
 variable "subnet_name" {
-  type = string
+  type    = string
   default = "bhs-subnet-01"
 }
 
 variable "publicip_name" {
-  type = string
+  type    = string
   default = "bhs-public-ip-01"
 }
 
