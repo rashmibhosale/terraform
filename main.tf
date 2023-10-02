afdaf4dbf844ae5382af8e1d1c7338a6dc6ad1ca
Rajeev Ranjan <Rajeev.Ranjan@cornerstone-bb.com>
2023-10-02 11:55:28 +0530

    code pushed for rg,vnet,subnet,pbip

---
+++ main.tf
@@ -0,0 +1,44 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.75.0"
+    }
+  }
+  # Defining the location remote satate file, so that multiple user can work together.
+  backend "azurerm" {
+    resource_group_name   = "backend-rg"
+    storage_account_name  = "terrastatebackend01"
+    container_name        = "terraformstate"
+    key                   = "bhs.terraform.tfstate"
+  }
+}
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+    name = var.resource_group_name
+    location = var.location
+}
+
+resource "azurerm_virtual_network" "vnet" {
+    name = var.vnet_name
+    resource_group_name = azurerm_resource_group.rg.name
+    location = azurerm_resource_group.rg.location
+    address_space = [ "10.0.0.0/16" ]
+}
+
+resource "azurerm_subnet" "subnet" {
+    name = var.subnet_name
+    resource_group_name = azurerm_resource_group.rg.name
+    virtual_network_name = azurerm_virtual_network.vnet.name
+    address_prefixes = [ "10.0.1.0/24" ]
+}
+
+resource "azurerm_public_ip" "pubip" {
+    name = var.publicip_name
+    location = azurerm_resource_group.rg.location
+    resource_group_name = azurerm_resource_group.rg.name
+    allocation_method = "Static"
+}
