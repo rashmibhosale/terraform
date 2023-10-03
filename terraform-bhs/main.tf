903c3be0d992730d6a5ed184d45bd3ba0e8bd402
Rajeev Ranjan <Rajeev.Ranjan@cornerstone-bb.com>
2023-10-03 12:10:40 +0530

    public ip added

--- terraform-bhs/main.tf
+++ terraform-bhs/main.tf
@@ -41,6 +41,7 @@ resource "azurerm_public_ip" "pubip" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   allocation_method   = "Static"
+  domain_name_label = "bhs"
 }
 
 resource "azurerm_network_security_group" "nsg" {
