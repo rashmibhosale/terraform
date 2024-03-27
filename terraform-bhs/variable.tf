2a8e8f483ebc12498033f96d960225e89e310e83
Rajeev Ranjan <85291867+rajeev107@users.noreply.github.com>
2024-03-27 14:04:01 +0530

    Update variable.tf

--- terraform-bhs/variable.tf
+++ terraform-bhs/variable.tf
@@ -1,6 +1,6 @@
 variable "resource_group_name" {
-  default     = "bhs-assignment-rg-01"
-  description = "This resource group is used to create resource for BHS"
+  default     = "demo-rg-01"
+  description = "This resource group is used to create resource for demo"
   type        = string
 }
 
@@ -11,30 +11,30 @@ variable "location" {
 
 variable "vnet_name" {
   type    = string
-  default = "bhs-vnet-01"
+  default = "demo-vnet-01"
 }
 
 variable "subnet_name" {
   type    = string
-  default = "bhs-subnet-01"
+  default = "demo-subnet-01"
 }
 
 variable "publicip_name" {
   type    = string
-  default = "bhs-public-ip-01"
+  default = "demo-public-ip-01"
 }
 
 variable "nsg_name" {
   type = string
-  default = "bhs-nsg-01"
+  default = "demo-nsg-01"
 }
 
 variable "nic_name" {
   type = string
-  default = "bhs-nic-01"
+  default = "demo-nic-01"
 }
 
 variable "linux_vm" {
   type = string
-  default = "bhs-linux-vm-01"
+  default = "demo-linux-vm-01"
 }
