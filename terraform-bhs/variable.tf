4ffc2856e126c50c5bf16eeb5d1b871a53b63a22
rashmibhosale <142342852+rashmibhosale@users.noreply.github.com>
2024-04-03 01:04:51 +0530

    Update variable.tf

--- terraform-bhs/variable.tf
+++ terraform-bhs/variable.tf
@@ -38,3 +38,24 @@ variable "linux_vm" {
   type = string
   default = "demo-linux-vm"
 }
+
+variable "role_name" {
+	type = string
+	default = "CustomRole"
+}
+
+variable "AZURE_SUBSCRIPTION_ID" {
+	type = string
+	default = "7ef199f3-53b2-4ae4-b178-56f7159813f8"
+}
+
+variable "role_actions" {
+	description = "List of actions allowed"
+	type = list(string)
+	default = [
+		"Microsoft.Compute/virtualMachines/read",
+		"Microsoft.Compute/virtualMachines/start/action",
+    "Microsoft.Compute/virtualMachines/deallocate/action",
+    "Microsoft.Storage/storageAccounts/listKeys/action"
+	]
+}
