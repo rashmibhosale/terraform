6ed2d89c901088575d9af6b578bb85b9912e82b4
Rajeev Ranjan <85291867+rajeev107@users.noreply.github.com>
2024-04-02 15:42:42 +0530

    Update main.tf

--- terraform-IaM/main.tf
+++ terraform-IaM/main.tf
@@ -1,7 +1,33 @@
 # In this file we define custom roles to perform action on  virtual machine.
-
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = ">= 1.0.0"
+    }
+  }
+  # Defining the location remote satate file, so that multiple user can work together.
+  backend "azurerm" {
+    resource_group_name  = "backend-rg"
+    storage_account_name = "terrastatebackend01"
+    container_name       = "terrastatefile"
+    key                  = "policy.terraform.tfstate"
+  }
+}
 provider "azurerm" {
   features{}  
+}
+
+provider "azuread" {
+  version = "~> 1.0"
+}
+
+data "azuread_group" "gp" {
+  name = "demopolicygroup"
 }
 
 resource "azurerm_role_definition" "custom-vm-permission" {
@@ -26,7 +52,7 @@ resource "azurerm_role_definition" "custom-vm-permission" {
 }
 
 resource "azurerm_role_assignment" "assignrole-to-rg" {
-  scope = "/subscription/${var.subscription_id}/resourceGroup/demorg"
+  scope = "/subscription/${var.subscription_id}/resourceGroup/demo-rg-01"
   role_definition_id = azurerm_role_definition.custom-vm-permission.id
-  principal_id = "f4a3a932-17f8-4970-b0e6-6bc5f427df7f"
+  principal_id = data.azuread_group.gp.id
 }
