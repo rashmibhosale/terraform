ff17e2045d35ce1e89587fce723c8e1a2d90e659
Rajeev Ranjan <85291867+rajeev107@users.noreply.github.com>
2024-04-03 12:06:26 +0530

    Update main.tf

--- terraform-AD/main.tf
+++ terraform-AD/main.tf
@@ -26,28 +26,43 @@ terraform {
 provider "azuread" {
 }
 
-
-data "azuread_domains" "tera-domain" {
-    only_initial = true
-  
+data "azuread_domains" "default" {
+  only_initial = true
 }
 
 locals {
-  domain_name = data.azuread_domains.tera-domain.domains.0.domain_name
-  users = csvdecode(file("${path.module}/userlist.csv"))
+  domain_name = data.azuread_domains.default.domains.0.domain_name
+  users        = csvdecode(file("${path.module}/userlist.csv"))
+  groups      = toset(local.users[*].department)
 }
 
 resource "azuread_user" "users" {
-  for_each = { for user in local.users : user.first_name => user}
-    password = format(
-        "%s%s%s!",
-        substr(lower(each.value.first_name),0,1),
-        lower(each.value.last_name),
-        length(each.value.last_name)
-    )
-    force_password_change = true
-    user_principal_name = "${each.value.first_name}@brashmi97gmail.onmicrosoft.com"
-    display_name = "${each.value.first_name} ${each.value.last_name}"
-    department = each.value.department
-    job_title = each.value.job-title
+  for_each = { for user in local.users : user.first_name => user }
+
+  user_principal_name = format(
+    "%s.%s@%s",
+    lower(each.value.first_name),
+    lower(each.value.last_name),
+    local.domain_name
+  )
+
+  password = format(
+    "%s%s%s!",
+    lower(each.value.last_name),
+    substr(lower(each.value.first_name), 0, 1),
+    length(each.value.first_name)
+  )
+  force_password_change = true
+
+  display_name = "${each.value.first_name} ${each.value.last_name}"
+  department   = each.value.department
+  job_title    = each.value.job_title
+}
+
+resource "azuread_group" "groups" {
+  for_each           = local.groups
+  display_name       = each.key
+  security_enabled   = true
+  assignable_to_role = true
+  members = [ for u in values(azuread_user.users) : u.id if u.department == each.key ]
 }
