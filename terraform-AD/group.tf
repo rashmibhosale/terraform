7c8aaa751dbf04c5f1f52373c85ce3b0d307beb1
Rajeev Ranjan <85291867+rajeev107@users.noreply.github.com>
2024-04-03 11:56:17 +0530

    Update group.tf

--- terraform-AD/group.tf
+++ terraform-AD/group.tf
@@ -7,7 +7,7 @@ resource "azuread_group" "department" {
 }
 
 resource "azuread_group_member" "department" {
-  for_each = { for user in azuread_user.users : user.value.first_name => user if user.department == "DevOps"}
+  for_each = { for user in azuread_user.users : user.first_name => user if user.department == "DevOps"}
 
   group_object_id = azuread_group.department.id
   member_object_id = each.value.id
@@ -20,7 +20,7 @@ resource "azuread_group" "Software" {
 }
 
 resource "azuread_group_member" "Software" {
-    for_each = { for user in azuread_user.users : user.value.first_name => user if user.job-title == "Software Engineer"}
+    for_each = { for user in azuread_user.users : user.first_name => user if user.job-title == "Software Engineer"}
 
     group_object_id = azuread_group.Software.id
     member_object_id = each.value.id
@@ -32,7 +32,7 @@ resource "azuread_group" "Manager" {
 }
 
 resource "azuread_group_member" "Manager" {
-  for_each = { for user in azuread_user.users : user.value.first_name => user if user.job-title == "Manager"}
+  for_each = { for user in azuread_user.users : user.first_name => user if user.job-title == "Manager"}
 
   group_object_id = azuread_group.Manager.id
   member_object_id = each.value.id
