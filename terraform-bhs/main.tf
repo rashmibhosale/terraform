b2b11f64b620cf58d63d99bf1978aa63d6b2cdf2
Rajeev Ranjan <85291867+rajeev107@users.noreply.github.com>
2024-04-04 12:44:35 +0530

    Update main.tf

--- terraform-bhs/main.tf
+++ terraform-bhs/main.tf
@@ -34,7 +34,6 @@ resource "azurerm_subnet" "subnet" {
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
-network_security_group_id = azurerm_network_security_group.nsg.id
 }
 
 resource "azurerm_public_ip" "pubip" {
