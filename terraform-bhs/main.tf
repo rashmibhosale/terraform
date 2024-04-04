84a60206e0d7d9bc093a96c10b78ec3c7f192320
Rajeev Ranjan <85291867+rajeev107@users.noreply.github.com>
2024-04-04 10:32:47 +0530

    count added in main.tf

--- terraform-bhs/main.tf
+++ terraform-bhs/main.tf
@@ -37,7 +37,7 @@ resource "azurerm_subnet" "subnet" {
 }
 
 resource "azurerm_public_ip" "pubip" {
-  count               = 2
+  count               = var.item_count
   name                = "${var.publicip_name}-${count.index}"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
@@ -63,7 +63,7 @@ resource "azurerm_network_security_group" "nsg" {
 }
 
 resource "azurerm_network_interface" "nic" {
-  count               = 2
+  count               = var.item_count
   name                = "${var.nic_name}-${count.index}"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
@@ -77,7 +77,7 @@ resource "azurerm_network_interface" "nic" {
 }
 
 resource "azurerm_linux_virtual_machine" "linvm" {
-  count               = 2
+  count               = var.item_count
   name                = "${var.linux_vm}-${count.index}"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
